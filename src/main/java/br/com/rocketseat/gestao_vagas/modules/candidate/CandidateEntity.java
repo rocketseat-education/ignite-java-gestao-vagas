bd831c219a2e0106c25e544980ba3ab67194bc38
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-10-07 15:07:38 -0300

    Criando tabela de candidato

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,18 +1,28 @@
 package br.com.rocketseat.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   private String name;
 
   @NotBlank
@@ -26,4 +36,7 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }

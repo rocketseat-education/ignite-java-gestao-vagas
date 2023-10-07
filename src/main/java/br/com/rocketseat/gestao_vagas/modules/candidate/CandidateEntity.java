1ca538e01e99c34b04eb7ca784480a0ccfe0dfa0
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-10-07 14:11:53 -0300

    Inserindo validação

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.rocketseat.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,14 @@ public class CandidateEntity {
 
   private UUID id;
   private String name;
+
+  @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
   private String username;
+
+  @Email(message = "O campo [email] deve conter um e-mail válido")
   private String email;
+
+  @Length(min = 10, max = 100)
   private String password;
   private String description;
   private String curriculum;
--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
   @PostMapping("/")
-  public void create(@RequestBody CandidateEntity candidateEntity) {
+  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
     System.out.println("Candidato");
     System.out.println(candidateEntity.getEmail());
   }

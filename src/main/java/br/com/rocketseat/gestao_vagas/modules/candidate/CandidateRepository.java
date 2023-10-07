c76cc30828a0e34896c61cc5bb124cd90da8fc62
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-10-07 15:12:06 -0300

    Criando repositório

---
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,9 @@
+package br.com.rocketseat.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+
+}
--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,20 +1,24 @@
 package br.com.rocketseat.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+  @Autowired
+  private CandidateRepository candidateRepository;
+
   @PostMapping("/")
-  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-    System.out.println("Candidato");
-    System.out.println(candidateEntity.getEmail());
+  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    return this.candidateRepository.save(candidateEntity);
   }
 }

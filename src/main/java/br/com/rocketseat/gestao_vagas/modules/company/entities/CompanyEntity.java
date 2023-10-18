c153697b2aa2ec1d02b20416c88b52dee0566393
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-10-18 15:08:46 -0300

    Criando entidade Company

---
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,42 @@
+package br.com.rocketseat.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @NotBlank
+  @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+  private String username;
+
+  @Email(message = "O campo [email] deve conter um e-mail válido")
+  private String email;
+
+  @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
+  private String password;
+
+  private String website;
+  private String name;
+  private String description;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}

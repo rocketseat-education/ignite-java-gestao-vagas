4d160bf74911e8acd86792fe363e09012de732d2
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-10-18 15:41:13 -0300

    Company Controller

---
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -0,0 +1,31 @@
+package br.com.rocketseat.gestao_vagas.modules.company.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rocketseat.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.rocketseat.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+  @Autowired
+  private CreateCompanyUseCase createCompanyUseCase;
+
+  @PostMapping("/")
+  public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity) {
+
+    try {
+      var result = this.createCompanyUseCase.execute(companyEntity);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+}

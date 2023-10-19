cdd11b2af71d92c4119d0ba0a72d4950ea8ff34f
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-10-19 11:12:20 -0300

    Spring Security

---
+++ src/main/java/br/com/rocketseat/gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package br.com.rocketseat.gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable());
+    return http.build();
+  }
+}

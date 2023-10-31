96ead8f712510882414522587f2353d6252a0cde
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-10-31 14:13:16 -0300

    Filtro de segurança do candidato

--- src/main/java/br/com/rocketseat/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/security/SecurityConfig.java
@@ -15,6 +15,9 @@ public class SecurityConfig {
   @Autowired
   private SecurityFilter securityFilter;
 
+  @Autowired
+  private SecurityCandidateFilter securityCandidateFilter;
+
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable())
@@ -24,7 +27,9 @@ public class SecurityConfig {
               .requestMatchers("/auth/company").permitAll()
               .requestMatchers("/candidate/auth").permitAll();
           auth.anyRequest().authenticated();
-        }).addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+        })
+        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
+        .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class);
     return http.build();
   }
 

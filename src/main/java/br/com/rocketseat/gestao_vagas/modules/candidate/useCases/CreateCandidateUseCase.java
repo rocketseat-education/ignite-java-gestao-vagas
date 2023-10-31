823fd56efc4c3c39bc76d0c5be8821b5bfdcbf65
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-10-31 13:19:30 -0300

    Hash da senha do candidato

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.rocketseat.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.rocketseat.gestao_vagas.exceptions.UserFoundException;
@@ -12,11 +13,17 @@ public class CreateCandidateUseCase {
   @Autowired
   private CandidateRepository candidateRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public CandidateEntity execute(CandidateEntity candidateEntity) {
     this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(),
         candidateEntity.getEmail()).ifPresent(user -> {
           throw new UserFoundException();
         });
+
+    var password = passwordEncoder.encode(candidateEntity.getPassword());
+    candidateEntity.setPassword(password);
 
     return this.candidateRepository.save(candidateEntity);
   }

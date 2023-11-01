db427235b65a9fd2d5989a0c3c482ae637967ffb
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-11-01 14:45:18 -0300

    Configurando o Swagger

--- src/main/java/br/com/rocketseat/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/GestaoVagasApplication.java
@@ -3,7 +3,11 @@ package br.com.rocketseat.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestão de Vagas", description = "API responsável pela gestão de vagas", version = "1"))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {

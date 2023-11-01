b70ac40d2fc7eadf91136dd5c7655da5eca8eb00
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-11-01 16:09:26 -0300

    Inserindo exemplos nas requisições

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,11 +1,18 @@
 package br.com.rocketseat.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
 
+  @Schema(example = "Vaga para pessoa desenvolvedora júnior", requiredMode = RequiredMode.REQUIRED)
   private String description;
+
+  @Schema(example = "GymPass, Plano de saúde", requiredMode = RequiredMode.REQUIRED)
   private String benefits;
+
+  @Schema(example = "JUNIOR", requiredMode = RequiredMode.REQUIRED)
   private String level;
 }

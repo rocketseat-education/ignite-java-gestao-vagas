abf126dfd5de1dc2c293ed544c8af01999fab4d2
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-11-01 15:09:47 -0300

    UseCase: ListAllJobsByFilter

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.rocketseat.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.rocketseat.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.rocketseat.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.rocketseat.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.rocketseat.gestao_vagas.modules.company.entities.JobEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -25,6 +29,9 @@ public class CandidateController {
 
   @Autowired
   private ProfileCandidateUseCase profileCandidateUseCase;
+
+  @Autowired
+  private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
   @PostMapping("/")
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -48,5 +55,11 @@ public class CandidateController {
     } catch (Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
+  }
+
+  @GetMapping("/job")
+  @PreAuthorize("hasRole('CANDIDATE')")
+  public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+    return this.listAllJobsByFilterUseCase.execute(filter);
   }
 }
---
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
@@ -0,0 +1,20 @@
+package br.com.rocketseat.gestao_vagas.modules.candidate.useCases;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rocketseat.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.rocketseat.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class ListAllJobsByFilterUseCase {
+
+  @Autowired
+  private JobRepository jobRepository;
+
+  public List<JobEntity> execute(String filter) {
+    return this.jobRepository.findByDescriptionContaining(filter);
+  }
+}

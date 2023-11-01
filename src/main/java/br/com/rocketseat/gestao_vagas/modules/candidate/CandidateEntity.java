811a83a78dea64234c9996fc9bcd262dbb881135
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-11-01 16:45:13 -0300

    Refatorando Documentação

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,17 +25,23 @@ public class CandidateEntity {
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
+  @Schema(example = "Daniel de Souza", requiredMode = RequiredMode.REQUIRED, description = "Nome do candidato")
   private String name;
 
   @NotBlank
   @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+  @Schema(example = "daniel", requiredMode = RequiredMode.REQUIRED, description = "Username do candidato")
   private String username;
 
   @Email(message = "O campo [email] deve conter um e-mail válido")
+  @Schema(example = "daniel@gmail.com", requiredMode = RequiredMode.REQUIRED, description = "E-mail do candidato")
   private String email;
 
   @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
+  @Schema(example = "admin@1234", minLength = 10, maxLength = 100, requiredMode = RequiredMode.REQUIRED, description = "Senha do candidato")
   private String password;
+
+  @Schema(example = "Desenvolvedor Java", requiredMode = RequiredMode.REQUIRED, description = "Breve descrição do candidato")
   private String description;
   private String curriculum;
 

d7dcfab80cdad5543a6a9f8b5359fbc81041ede0
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-11-01 16:09:26 -0300

    Inserindo exemplos nas requisições

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/controllers/JobController.java
@@ -12,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.rocketseat.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.rocketseat.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.rocketseat.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -24,6 +31,14 @@ public class JobController {
 
   @PostMapping("/")
   @PreAuthorize("hasRole('COMPANY')")
+  @Tag(name = "Vagas", description = "Informações das vagas")
+  @Operation(summary = "Cadastro de vaga", description = "Essa função é responsável por cadastrar as vagas dentro da empresa")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", content = {
+          @Content(schema = @Schema(implementation = JobEntity.class))
+      })
+  })
+  @SecurityRequirement(name = "jwt_auth")
   public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
     var companyId = request.getAttribute("company_id");
 
--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -28,10 +29,15 @@ public class JobEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Schema(example = "Vaga para design")
   private String description;
+
+  @Schema(example = "GymPass, Plano de saúde")
   private String benefits;
 
   @NotBlank(message = "Esse campo é obrigatório")
+  @Schema(example = "SENIOR")
   private String level;
 
   @ManyToOne()

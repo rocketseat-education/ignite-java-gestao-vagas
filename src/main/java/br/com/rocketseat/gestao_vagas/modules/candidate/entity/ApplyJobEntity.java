762f224a86c2db2687c3b022ddb85e5f8a6b16ee
vhraposo <thevhup@gmail.com>
2023-11-18 14:24:37 -0300

    Criando Repositório Apply Job

---
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/entity/ApplyJobEntity.java
@@ -0,0 +1,49 @@
+package br.com.rocketseat.gestao_vagas.modules.candidate.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.rocketseat.gestao_vagas.modules.company.entities.JobEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "apply_jobs")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApplyJobEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "candidate_id", insertable = false, updatable = false)
+    private CandidateEntity candidateEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id", insertable = false, updatable = false)
+    private JobEntity jobEntity;
+
+    @Column(name = "candidate_id")
+    private UUID candidateId;
+
+    @Column(name = "job_id")
+    private UUID jobId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import br.com.rocketseat.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.rocketseat.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.rocketseat.gestao_vagas.modules.candidate.repository.ApplyJobRepository;
 import br.com.rocketseat.gestao_vagas.modules.company.repositories.JobRepository;
 
 @Service
@@ -18,6 +19,9 @@ public class ApplyJobCandidateUseCase {
 
     @Autowired
     private JobRepository jobRepository;
+
+    @Autowired
+    private ApplyJobRepository applyJobRepository;
     
     // ID do candidato
     // ID da vaga

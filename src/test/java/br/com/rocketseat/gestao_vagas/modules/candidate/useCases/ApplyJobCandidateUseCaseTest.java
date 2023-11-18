18385c24bbfa96c60ebccb7a5ce2d9a73eef90b9
vhraposo <thevhup@gmail.com>
2023-11-18 13:57:06 -0300

    Criando teste JobNotFound

--- src/test/java/br/com/rocketseat/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/br/com/rocketseat/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -1,6 +1,10 @@
 package br.com.rocketseat.gestao_vagas.modules.candidate.useCases;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +13,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.rocketseat.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.rocketseat.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.rocketseat.gestao_vagas.modules.company.repositories.JobRepository;
 
@@ -32,6 +38,22 @@ public class ApplyJobCandidateUseCaseTest {
             applyJobCandidateUseCase.execute(null, null);
         } catch (Exception e){
             assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+
+    @Test
+    public void should_not_be_able_to_apply_job_with_job_not_found(){
+        var idCandidate = UUID.randomUUID();
+
+        var candidate = new CandidateEntity();
+        candidate.setId(idCandidate);
+
+        when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+
+        try{
+            applyJobCandidateUseCase.execute(idCandidate, null);
+        } catch (Exception e){
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
         }
     }
 }

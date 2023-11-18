93c58d284d67f0b7975b7209353d92ebb002f058
vhraposo <thevhup@gmail.com>
2023-11-17 23:56:31 -0300

    UseCase: ApplyJob

---
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -0,0 +1,39 @@
+package br.com.rocketseat.gestao_vagas.modules.candidate.useCases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rocketseat.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.rocketseat.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.rocketseat.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class ApplyJobCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+    
+    // ID do candidato
+    // ID da vaga
+    public void execute(UUID idCandidate, UUID idJob){
+        // Validar se o candidato existe
+        this.candidateRepository.findById(idCandidate)
+        .orElseThrow(() -> {
+            throw new UserNotFoundException();
+        });
+
+        // Validar se a vaga existe
+        this.jobRepository.findById(idJob)
+        .orElseThrow(() -> {
+            throw new JobNotFoundException();
+        });
+
+        // Candidato se inscrever na vaga
+    }
+}

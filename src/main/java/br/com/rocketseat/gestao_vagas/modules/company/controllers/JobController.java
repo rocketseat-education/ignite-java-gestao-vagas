67910d9e764dbc806124086426908046a23d12cb
vhraposo <thevhup@gmail.com>
2023-11-20 11:28:48 -0300

    Criando Test Controller Exception - PT2

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/controllers/JobController.java
@@ -54,7 +54,7 @@ public class JobController {
         var result = this.createJobUseCase.execute(jobEntity);
         return ResponseEntity.ok().body(result);
         }catch(Exception e){
-                return ResponseEntity.badRequest().body(e);
+                return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 

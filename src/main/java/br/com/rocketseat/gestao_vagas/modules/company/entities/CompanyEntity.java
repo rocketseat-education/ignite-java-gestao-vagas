dfafda674e9658fe9810abe15c767f8a7ed8ed8e
vhraposo <thevhup@gmail.com>
2023-11-20 10:41:27 -0300

    Implementando banco de dados h2

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -13,10 +13,16 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "company")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompanyEntity {
 
   @Id

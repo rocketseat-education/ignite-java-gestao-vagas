ca3e3f7a429aa261943c02eab45ee1a31c7bef88
vhraposo <thevhup@gmail.com>
2023-11-19 22:28:52 -0300

    Criando Test Controller - pt1

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -2,9 +2,15 @@ package br.com.rocketseat.gestao_vagas.modules.company.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateJobDTO {
 
   @Schema(example = "Vaga para pessoa desenvolvedora júnior", requiredMode = RequiredMode.REQUIRED)

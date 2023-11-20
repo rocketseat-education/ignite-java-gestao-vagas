9fc9b95c6e941e9c3ff922432cccd6868c1884a0
vhraposo <thevhup@gmail.com>
2023-11-19 21:49:29 -0300

    fix: autowired return bug at job controller and use case

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -1,5 +1,6 @@
 package br.com.rocketseat.gestao_vagas.modules.company.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.rocketseat.gestao_vagas.modules.company.entities.JobEntity;
@@ -7,9 +8,11 @@ import br.com.rocketseat.gestao_vagas.modules.company.repositories.JobRepository
 
 @Service
 public class CreateJobUseCase {
-  private JobRepository jobRepository;
 
-  public JobEntity execute(JobEntity jobEntity) {
-    return this.jobRepository.save(jobEntity);
-  }
+    @Autowired
+    private JobRepository jobRepository;
+
+    public JobEntity execute(JobEntity jobEntity) {
+        return this.jobRepository.save(jobEntity);
+    }
 }

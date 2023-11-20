2bc0364cd663cfd46f7e1eb0b403fc57e8af5b85
vhraposo <thevhup@gmail.com>
2023-11-20 11:21:04 -0300

    Criando Test Controller Exception

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/controllers/JobController.java
@@ -3,6 +3,7 @@ package br.com.rocketseat.gestao_vagas.modules.company.controllers;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,17 +40,22 @@ public class JobController {
             })
     })
     @SecurityRequirement(name = "jwt_auth")
-    public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
         var companyId = request.getAttribute("company_id");
 
-        var jobEntity = JobEntity.builder()
+        try{
+                var jobEntity = JobEntity.builder()
                 .benefits(createJobDTO.getBenefits())
                 .companyId(UUID.fromString(companyId.toString()))
                 .description(createJobDTO.getDescription())
                 .level(createJobDTO.getLevel())
                 .build();
 
-        return this.createJobUseCase.execute(jobEntity);
+        var result = this.createJobUseCase.execute(jobEntity);
+        return ResponseEntity.ok().body(result);
+        }catch(Exception e){
+                return ResponseEntity.badRequest().body(e);
+        }
     }
 
 }

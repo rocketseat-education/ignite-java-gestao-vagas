933b6cf7e4ee8450391a7bbc306b856f673e9509
vhraposo <thevhup@gmail.com>
2023-12-05 22:09:43 -0300

    Inserindo actuator

--- src/main/java/br/com/rocketseat/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/security/SecurityConfig.java
@@ -20,10 +20,11 @@ public class SecurityConfig {
   @Autowired
   private SecurityCandidateFilter securityCandidateFilter;
 
-  private static final String[] SWAGGER_LIST = {
+  private static final String[] PERMIT_ALL_LIST = {
       "/swagger-ui/**",
       "/v3/api-docs/**",
       "/swagger-resource/**",
+      "/actuator/**"
   };
 
   @Bean
@@ -34,7 +35,7 @@ public class SecurityConfig {
               .requestMatchers("/company/").permitAll()
               .requestMatchers("/company/auth").permitAll()
               .requestMatchers("/candidate/auth").permitAll()
-              .requestMatchers(SWAGGER_LIST).permitAll();
+              .requestMatchers(PERMIT_ALL_LIST).permitAll();
           auth.anyRequest().authenticated();
         })
         .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)

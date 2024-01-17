fe5430cb49ce06fd77f9807ab143c497823f4dd3
vhraposo <thevhup@gmail.com>
2024-01-17 20:50:57 -0300

    Adicionando token no spring security e na sessão (back-end)

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -1,5 +1,7 @@
 package br.com.rocketseat.gestao_vagas.modules.candidate.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +12,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthCandidateResponseDTO {
+
   private String access_token;
   private Long expires_in;
+  private List<String> roles;
 }

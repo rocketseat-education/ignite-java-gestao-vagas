ce2cf9a470b7e8b9daddbbc64b0fc0d03ae8dae7
vhraposo <thevhup@gmail.com>
2024-03-12 10:15:19 -0300

    Adicionando role company

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/dto/AuthCompanyResponseDTO.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/dto/AuthCompanyResponseDTO.java
@@ -1,5 +1,7 @@
 package br.com.rocketseat.gestao_vagas.modules.company.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,4 +14,5 @@ import lombok.NoArgsConstructor;
 public class AuthCompanyResponseDTO {
   private String access_token;
   private Long expires_in;
+  private List<String> roles;
 }
